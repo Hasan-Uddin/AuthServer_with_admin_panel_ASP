e56c9b9ac36d79dd88d1043a0aa75b1e64cbed80
Hasan Uddin <abcnizamd@gmail.com>
2025-11-24 23:32:08 +0600

    result.cs copied from dev to solve merge conflict

--- src/SharedKernel/Result.cs
+++ src/SharedKernel/Result.cs
@@ -17,22 +17,19 @@ public class Result
     }
 
     public bool IsSuccess { get; }
-
     public bool IsFailure => !IsSuccess;
-
     public Error Error { get; }
 
     public static Result Success() => new(true, Error.None);
-
-    public static Result<TValue> Success<TValue>(TValue value) =>
-        new(value, true, Error.None);
-
+    public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
     public static Result Failure(Error error) => new(false, error);
+    public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
 
-    public static Result<TValue> Failure<TValue>(Error error) =>
-        new(default, false, error);
+    public static Result<TValue> Failure<TValue>(string errorMessage) =>
+        new(default, false, Error.Failure("General.Error", errorMessage));
 
-
+    public static Result Failure(string errorMessage) =>
+        new(false, Error.Failure("General.Error", errorMessage));
 }
 
 public class Result<TValue> : Result
@@ -52,7 +49,4 @@ public class Result<TValue> : Result
 
     public static implicit operator Result<TValue>(TValue? value) =>
         value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
-
-    public static Result<TValue> ValidationFailure(Error error) =>
-        new(default, false, error);
 }
