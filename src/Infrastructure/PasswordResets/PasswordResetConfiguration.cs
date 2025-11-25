82b86cceef79b50c420d7413415eee90963d4eee
MashrafeJess <mashrafeiubat@gmail.com>
2025-11-25 17:01:25 +0600

    token

--- src/Infrastructure/PasswordResets/PasswordResetConfiguration.cs
+++ src/Infrastructure/PasswordResets/PasswordResetConfiguration.cs
@@ -8,8 +8,8 @@ public class PasswordResetConfiguration : IEntityTypeConfiguration<PasswordReset
     public void Configure(EntityTypeBuilder<PasswordReset> builder)
     {
         builder.HasKey(c => c.PrId);
-        builder.Property(c => c.Token).IsRequired().HasMaxLength(255);
         builder.Property(c => c.UserId).IsRequired();
+        builder.Property(c => c.Token).IsRequired().HasMaxLength(255);
         builder.Property(c => c.ExpiresAt).IsRequired();
         builder.Property(c => c.Used);
     }
