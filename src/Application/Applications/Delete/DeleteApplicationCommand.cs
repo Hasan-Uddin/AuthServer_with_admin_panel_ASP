3f705d2a5d8d0af4ee8fec950a7d1e7e5a43fbb2
Hasan Uddin <abcnizamd@gmail.com>
2025-12-14 23:44:33 +0600

    remove Applications ⚰

--- src/Application/Applications/Delete/DeleteApplicationCommand.cs
+++
@@ -1,4 +0,0 @@
-﻿using Application.Abstractions.Messaging;
-
-namespace Application.Applications.Delete;
-public sealed record DeleteApplicationCommand(Guid Id) : ICommand;
--- src/Application/Applications/GetAll/GetAllApplicationsQuery.cs
+++
@@ -1,5 +0,0 @@
-﻿using Application.Abstractions.Messaging;
-using Application.Applications.Get;
-
-namespace Application.Applications.GetAll;
-public sealed record GetAllApplicationsQuery() : IQuery<List<ApplicationResponse>>;
