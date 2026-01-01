4d5837d9e0e7fc110b531006c25678dc39ecf9e0
Hasan Uddin <abcnizamd@gmail.com>
2026-01-01 19:18:36 +0600

    admin check

---
+++ src/Web.Api/Endpoints/Auth/AdminCheck.cs
@@ -0,0 +1,24 @@
+﻿using Application.Abstractions.Authentication;
+using Domain.Roles;
+using Web.Api.Extensions;
+
+namespace Web.Api.Endpoints.Auth;
+
+internal sealed class AdminCheck : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("auth/admin-check", async (
+            IUserContext userContext,
+            CancellationToken cancellationToken) =>
+        {
+            if (!userContext.IsAuthenticated)
+            {
+                return Results.Unauthorized();
+            }
+            return Results.NoContent();
+        })
+        .WithTags(Tags.Users)
+        .RequireRole(RoleCode.Admin);
+    }
+}
