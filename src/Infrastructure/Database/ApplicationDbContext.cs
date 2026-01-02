667d5d2af800b85fa709ecc998088fda6894244d
Hasan Uddin <abcnizamd@gmail.com>
2026-01-02 15:20:29 +0600

    seed data extracted ✨

--- src/Infrastructure/Database/ApplicationDbContext.cs
+++ src/Infrastructure/Database/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ using Domain.SmtpConfigs;
 using Domain.Todos;
 using Domain.UserRoles;
 using Domain.Users;
+using Infrastructure.Database.Seed;
 using Infrastructure.DomainEvents;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
@@ -51,62 +52,7 @@ public sealed class ApplicationDbContext(
 
         modelBuilder.HasDefaultSchema(Schemas.Default);
 
-        modelBuilder.Entity<Role>().HasData(
-            new Role
-            {
-                Id = Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"),
-                RoleName = "Administrator",
-                RoleCode = RoleCode.Admin,
-                Description = "System Administrator with full access"
-            },
-            new Role
-            {
-                Id = Guid.Parse("bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"),
-                RoleName = "Support",
-                RoleCode = RoleCode.Support,
-                Description = "Support Engineers"
-            },
-            new Role
-            {
-                Id = Guid.Parse("cccccccc-cccc-cccc-cccc-cccccccccccc"),
-                RoleName = "Analytics",
-                RoleCode = RoleCode.Analytics,
-                Description = "Helps in Analysis"
-            },
-            new Role
-            {
-                Id = Guid.Parse("dddddddd-dddd-dddd-dddd-dddddddddddd"),
-                RoleName = "PaymentAdmin",
-                RoleCode = RoleCode.PaymentAdmin,
-                Description = "Asses the payments"
-            },
-            new Role
-            {
-                Id = Guid.Parse("eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee"),
-                RoleName = "Common Usser",
-                RoleCode = RoleCode.PublicUser,
-                Description = "Common/Normal User"
-            }
-        );
-        modelBuilder.Entity<User>().HasData(
-            new User
-            {
-                Id = Guid.Parse("ffffffff-ffff-ffff-ffff-ffffffffffff"),
-                Email = "admin@auth.dapplesoft.com",
-                FullName = "Default Admin",
-                PasswordHash = "60358AD3245A0E1D8FC2CA0B0914E45C5F87143DDB2C9E81E09B4E41676F30B8-99D093AF2C44DB8DDCA9FE77BDE4A9F2", // admin12345
-                CreatedAt = new DateTime(2025, 12, 16, 0, 0, 0, DateTimeKind.Utc),
-                UpdatedAt = new DateTime(2025, 12, 16, 0, 0, 0, DateTimeKind.Utc)
-            }
-        );
-        modelBuilder.Entity<UserRole>().HasData(
-            new UserRole
-            {
-                Id = Guid.Parse("aaaaaaaa-eeee-ffff-ffff-ffffffffffff"),
-                UserId = Guid.Parse("ffffffff-ffff-ffff-ffff-ffffffffffff"),
-                RoleId = Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"),
-            }
-        );
+        UsersAndRoleSeed.Apply(modelBuilder);
     }
 
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
---
+++ src/Infrastructure/Database/Seed/RegionsSeed.cs
@@ -0,0 +1,28 @@
+﻿
+using Domain.Countries;
+using Domain.Localities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database.Seed;
+
+internal static class RegionsSeed
+{
+    internal static void Apply(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Country>().HasData(Contries());
+        modelBuilder.Entity<Division>().HasData(Users());
+        modelBuilder.Entity<Locality>().HasData(UserRoles());
+    }
+
+    private static Country[] Contries() =>
+    [
+        new Country
+        {
+            Id = Guid.Parse("b2a16d2f-4512-452b-abfe-baa28ea9da59"),
+            Name = "Bangladesh",
+            Capital = "Dhaka",
+            PhoneCode = "+880",
+            IsActive = true
+        }
+    ];
+}
