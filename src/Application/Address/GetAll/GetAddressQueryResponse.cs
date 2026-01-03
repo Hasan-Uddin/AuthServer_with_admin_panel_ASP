810498f60c07a46fb4acea08f8e2662ea3991009
Hasan Uddin <abcnizamd@gmail.com>
2026-01-03 15:12:38 +0600

    Address Endpoint ✅✅

---
+++ src/Application/Address/GetAll/GetAddressQueryResponse.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Application.Address.GetAll;
+
+public sealed class GetAddressQueryResponse 
+{
+    public CountryDto Country { get; set; }
+}
+
+public class CountryDto
+{
+    [JsonPropertyOrder(1)]
+    public Guid Id { get; set; }
+
+    [JsonPropertyOrder(2)]
+    public string Name { get; set; } = null!;
+
+    [JsonPropertyOrder(3)]
+    public List<RegionDto>  Regions{ get; set; }
+}
+
+public class RegionDto
+{
+    [JsonPropertyOrder(1)]
+    public Guid Id { get; set; }
+
+    [JsonPropertyOrder(2)]
+    public string Name { get; set; } = null!;
+
+    [JsonPropertyOrder(3)]
+    public List<DistrictDto> Districts { get; set; }
+}
+
+public class DistrictDto
+{
+    [JsonPropertyOrder(1)]
+    public Guid Id { get; set; }
+
+    [JsonPropertyOrder(2)]
+    public string Name { get; set; } = null!;
+
+    [JsonPropertyOrder(3)]
+    public List<SubDistrictDto> SubDistricts { get; set; }
+}
+
+public class SubDistrictDto 
+{
+    [JsonPropertyOrder(1)]
+    public Guid Id { get; set; }
+
+    [JsonPropertyOrder(2)]
+    public string Name { get; set; } = null!;
+}
---
+++ src/Web.Api/Endpoints/Address/GetAll.cs
@@ -0,0 +1,24 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Address.GetAll;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Address;
+
+public class GetAll : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet(ApiRoutes.GetAll(Base.Addresses), async (
+                IQueryHandler<GetAddressQuery, List<GetAddressQueryResponse>> handler,
+                CancellationToken cancellationToken) =>
+        {
+            var query = new GetAddressQuery();
+
+            Result<List<GetAddressQueryResponse>> result = await handler.Handle(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        }).WithTags(Tags.Addresses);
+    }
+}
