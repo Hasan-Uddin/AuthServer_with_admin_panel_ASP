d6993a0f5ba9046813041c68add0737db383a197
Hasan Uddin <abcnizamd@gmail.com>
2026-01-17 20:00:49 +0600

    verifiy account endpoint

---
+++ src/Web.Api/Endpoints/Users/Verification/VerifyOtp.cs
@@ -0,0 +1,35 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Users.Verification.VerifyOtp;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Users.Verification;
+
+internal sealed class VerifyOtp : IEndpoint
+{
+    internal sealed class Request
+    {
+        public string Destination { get; set; }
+        public string OtpCode { get; set; } = null!;
+    }
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost(ApiRoutes.Create(Base.VerifyAccountOtp), async (
+            Request request,
+            ICommandHandler<VerifyOtpCommand> handler,
+            CancellationToken ct
+        ) =>
+        {
+            var command = new VerifyOtpCommand(
+                request.Destination,
+                request.OtpCode
+            );
+            Result result = await handler.Handle(command, ct);
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .WithTags(Tags.Users)
+        .RequireAuthorization();
+    }
+}
