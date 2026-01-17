a4401c1218116ad3707ef84ebb991fcd648ea037
Hasan Uddin <abcnizamd@gmail.com>
2026-01-17 20:00:02 +0600

    Request account Verification (using otp)

---
+++ src/Application/Users/Verification/RequstOtpVerify/RequestOtpVerifyResponse.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Application.Users.Verification.RequstOtpVerify;
+
+public sealed record RequestOtpVerifyResponse(
+    Guid Id,
+    string? Destination = null,
+    DateTime? ExpiresAt = null,
+    DateTime? CreatedAt = null,
+    string? Message = null,
+    double? Delay = 0,
+    int? WaitForSeconds = 0
+);
